5229092b613891f1dbb9441f077b5c4f4e640932
pheebemi <lemuelemmanuel29@gmail.com>
2026-05-15 17:09:27 +0100

    kz;z;cz

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,17 @@
 import sys
 import os
 
-# Add your project directory to the path
-sys.path.insert(0, os.path.dirname(__file__))
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(APP_DIR)
+sys.path.insert(0, APP_DIR)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'school_management.settings'
+try:
+    from dotenv import load_dotenv
+    load_dotenv(os.path.join(APP_DIR, '.env'))
+except Exception:
+    pass
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_management.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
