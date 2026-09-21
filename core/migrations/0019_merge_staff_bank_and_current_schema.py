1b5995261e79c35947661ba757b07493fcb74f4b
pheebemi <lemuelemmanuel29@gmail.com>
2026-09-21 14:40:19 +0100

    Resolve core migration branches

---
+++ core/migrations/0019_merge_staff_bank_and_current_schema.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0015_staff_account_number_staff_bank_name'),
+        ('core', '0018_alter_academicyear_id_alter_admissionfee_id_and_more'),
+    ]
+
+    operations = []
